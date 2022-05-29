45d39e912626f594aa4d183080858d776c02cf67
sp1d5r <aa1719@imperial.ac.uk>
2022-05-29 19:16:42 +0100

    Front-end: Navigation Bar support for mobile.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,11 @@
 import './navbar.css';
-import React from "react";
+import React, {useState} from "react";
 import Button from "../button/button";
 
 function NavBar() {
+    const [opened, updateOpened] = useState(false);
+
+
     return (
         <div className={"navbar"}>
             <div className={"brand"}>
@@ -10,25 +13,48 @@ function NavBar() {
                     My Logo
                 </p>
             </div>
-            <div className={"navbar-items"}>
-                <div className={"navbar-links"}>
-                    <a className={"navbar-link"}>
-                        Documentation
-                    </a>
-                    <a className={"navbar-link"}>
-                        Academy
-                    </a>
-                    <a className={"navbar-link"}>
-                        Contact Us
-                    </a>
-                    <a className={"navbar-link"}>
-                        About Us
-                    </a>
+            {
+                window.innerWidth > 600 ?
+                    <div className={"navbar-items"}>
+                        <div className={"navbar-links"}>
+                            <a className={"navbar-link"}>
+                                Documentation
+                            </a>
+                            <a className={"navbar-link"}>
+                                Academy
+                            </a>
+                            <a className={"navbar-link"}>
+                                Contact Us
+                            </a>
+                            <a className={"navbar-link"}>
+                                About Us
+                            </a>
+                        </div>
+                        <div className={"navbar-button"}>
+                            <Button text={"Login"}/>
+                        </div>
+                    </div>
+                    :
+                    <div className={"navbar-items"}>
+                        <button className={"image-button"} onClick={() => updateOpened(!opened)}>
+                            <img className={"nav-image"} src={require("../../assets/Stack.png")} alt={"stack"}/>
+                        </button>
+                    </div>
+            }
+            {opened ?
+                <div id="myNav" className="overlay">
+
+                    <div className="overlay-content">
+                        <a className={"overlay-link"} href="#">Documentation</a>
+                        <a className={"overlay-link"} href="#">Academy</a>
+                        <a className={"overlay-link"} href="#">Contact Us</a>
+                        <a className={"overlay-link"} href="#">About Us</a>
+                    </div>
+
                 </div>
-                <div className={"navbar-button"}>
-                    <Button text={"Login"} />
-                </div>
-            </div>
+                :
+                <div />
+            }
         </div>
     );
 }
