ccf16a51d1e8e935d214902ffe876149ad7fb24b
sp1d5r <aa1719@imperial.ac.uk>
2022-06-03 13:21:59 +0100

    Front-end: Added routing and authentication screen for login.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import "./App.css";
 import NavBar from "./components/navbar/navbar";
 import React from "react";
-import Jumbotron from "./components/landing/Jumbotron/jumbotron";
-import Why from "./components/landing/Why/why";
-import Help from "./components/landing/Help/help";
-import How from "./components/landing/How/how";
-import PricingPlan from "./components/landing/Pricing/pricing-plan";
-import EmotionalIntelligence from "./components/landing/Emotional Intelligence/emotional_intelligence";
 import Footer from "./components/footer/footer";
+import Landing from "./components/landing/landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authenticate from "./components/auth/authenticate";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Jumbotron />
-      <Why />
-      <Help />
-      <How />
-      <EmotionalIntelligence />
-      <PricingPlan />`
-      <div className={"what-are-you-waiting-for"}>
-        <p>What are you waiting for?</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Authenticate />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/components/auth/login/login.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "./login.css";
+import GreenButton from "../../button/green-button";
+
+function Login({ changeObjective }) {
+  return (
+    <div className={"login-home"}>
+      <div className={"login-top"}>
+        <p>Login</p>
+      </div>
+      <div className={"login-bottom"}>
+        <div className={"login-left"}>
+          <p className={"login-main-text"}>Login to Your Account</p>
+          <div className={"login-forum"}>
+            <p>Email Address</p>
+            <input></input>
+            <p>Password</p>
+            <input></input>
+            <br />
+            <br />
+            <GreenButton text={"Login"} style={{ width: "20%" }} />
+            <div className={"login-button"}>
+              <div className={"login-bar"} />
+              <p>Or Continue With</p>
+              <div className={"login-bar"} />
+            </div>
+            <div className={"login-button"}>
+              <div className={"alternative-sign-in"}>
+                <img
+                  className={"alternative-sign-in-image "}
+                  src={require("../../../assets/Icons/Microsoft-Cloud.png")}
+                  alt={"Login with Microsoft"}
+                />
+              </div>
+              <div className={"alternative-sign-in"}>
+                <img
+                  className={"alternative-sign-in-image "}
+                  src={require("../../../assets/Icons/Google-Cloud.png")}
+                  alt={"Login with Google"}
+                />
+              </div>
+              <div className={"alternative-sign-in"}>
+                <img
+                  className={"alternative-sign-in-image "}
+                  src={require("../../../assets/Icons/Facebook-Cloud.png")}
+                  alt={"Login with Facebook"}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={"login-right"}>
+          <div className={"login-right-main"}>
+            <img
+              className={"login-page-illustration"}
+              src={require("../../../assets/Illustrations/login.png")}
+              alt={"Login illustration"}
+            />
+            <span>
+              Or switch to{" "}
+              <span
+                className={"switch-text-bold"}
+                onClick={() => changeObjective(false)}
+              >
+                Sign Up
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
