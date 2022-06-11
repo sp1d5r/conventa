4e7ead924b308fdcb75a8f301b15a7428cb4c53b
sp1d5r <aa1719@imperial.ac.uk>
2022-06-11 19:34:57 +0100

    Front-end: Added partial minigame - First Impressions.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Authenticate from "./components/auth/authenticate";
 import Academy from "./components/academy/academy";
 import CourseLanding from "./components/course-page/course-landing";
 import LessonPage from "./components/lesson/lesson-page";
+import FirstImpressions from "./components/minigames/first-impressions/first-impressions";
 
 function App() {
   return (
@@ -19,6 +20,10 @@ function App() {
           <Route path="/academy" element={<Academy loggedIn={false} />} />
           <Route path="/course/" element={<CourseLanding />} />
           <Route path="/lesson/" element={<LessonPage />} />
+          <Route
+            path="/mini-games/first-impressions/"
+            element={<FirstImpressions />}
+          />
           <Route path="/" element={<Landing />} />
         </Routes>
         <Footer />
---
+++ src/components/minigames/first-impressions/first-impressions.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./first-impressions.css";
+import StartFirstImpressions from "./start-first-impressions/start-first-impressions";
+
+function FirstImpressions() {
+  const [state, setState] = useState(0);
+
+  const from_minigame_state = () => {
+    if (state === 0) {
+      /* This is the intro state */
+      return <StartFirstImpressions />;
+    } else if (state === 1) {
+      /* This is the game state */
+      setState(1);
+    } else {
+      /* This is the game complete state */
+    }
+  };
+
+  return (
+    <div className={"first-impressions-main-div"}>{from_minigame_state()}</div>
+  );
+}
+
+export default FirstImpressions;
