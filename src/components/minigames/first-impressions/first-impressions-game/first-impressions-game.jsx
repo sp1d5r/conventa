ae4a699a8ff823b3f5858262ebc5bc69e0df83a1
sp1d4r <aa1719@imperial.ac.uk>
2022-06-28 21:15:48 +0100

    Front-end: Added a timer to the minigame section.

--- src/components/minigames/first-impressions/first-impressions-game/first-impressions-game.jsx
+++ src/components/minigames/first-impressions/first-impressions-game/first-impressions-game.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import "../first-impressions.css";
 import MinigameButton from "../../../button/minigame-button";
 
-function FirstImpressionsGame({ setState }) {
+
+function FirstImpressionsGame({  }) {
+  const timeRef = useRef(10);
+  const [text, setText] = useState("")
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timeRef.current == 0){
+        timeRef.current = 11;
+        setText("new game")
+      } else {
+        setText(`Time remaining ${timeRef.current}`);
+      }
+      timeRef.current--;
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
     <div className={"first-impressions-card"}>
       <p className={"first-impressions-title"}>Minigame - First Impressions</p>
       <div className={"first-impressions-info"}>
+      <p>{text}</p>
       </div>
     </div>
   );
