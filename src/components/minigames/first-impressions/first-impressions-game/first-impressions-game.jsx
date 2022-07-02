b4f34b6df4a0b7b9059854d951a632634d214b75
sp1d4r <aa1719@imperial.ac.uk>
2022-07-02 11:29:46 +0100

    Front-end: Added infobox items on first impressions game.

--- src/components/minigames/first-impressions/first-impressions-game/first-impressions-game.jsx
+++ src/components/minigames/first-impressions/first-impressions-game/first-impressions-game.jsx
@@ -5,17 +5,21 @@ import MinigameButton from "../../../button/minigame-button";
 
 function FirstImpressionsGame({  }) {
   const timeRef = useRef(10);
+  const paused = useRef(false);
   const [text, setText] = useState("")
+
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (timeRef.current == 0){
-        timeRef.current = 11;
-        setText("new game")
-      } else {
-        setText(`Time remaining ${timeRef.current}`);
+      if (!paused.current) {
+        if (timeRef.current == 0){
+          timeRef.current = 11;
+          setText("new game")
+        } else {
+          setText(`Time remaining ${timeRef.current}`);
+        }
+        timeRef.current--;
       }
-      timeRef.current--;
     }, 1000);
     return () => clearInterval(interval);
   }, []);
@@ -25,7 +29,10 @@ function FirstImpressionsGame({  }) {
     <div>
     <div className={"first-impressions-game-title"}>
     <p>Minigame - First Impressions</p>
-    <button> pause </button>
+    <div className={"first-impressions-menu"}>
+      <button onClick={() => {paused.current = !paused.current}}> Pause</button>
+      <button> skip </button>
+    </div>
     </div>
     <div className={"first-impressions-game-cards"}>
       <div className={"first-impressions-card-main"}>
@@ -33,21 +40,40 @@ function FirstImpressionsGame({  }) {
 
         </div>
         <div className={"first-impressions-infobox"}>
-        <p>Time remaining: <b>{timeRef.current}</b></p>
+          <div className={"infobox-left"}>
+            <div className={"inline-objects"}>
+              <p>Image</p>
+              <p>Time remaining: <b>{timeRef.current}</b></p>
+            </div>
+            <div className={"inline-objects"}>
+              <p>Image</p>
+              <p>Time remaining: <b>{timeRef.current}</b></p>
+            </div>
+          </div>
+          <div className={"infobox-left"}>
+            <div className={"inline-objects"}>
+              <p>Image</p>
+              <p>Tasks remaining: <b>{timeRef.current}</b></p>
+            </div>
+            <div className={"inline-objects"}>
+              <p>Image</p>
+              <p>Tasks remaining: <b>{timeRef.current}</b></p>
+            </div>
+          </div>
         </div>
       </div>
       <div className={"first-impressions-cards-right"}>
         <div className={"first-impressions-card-right"}>
-
+          <p>Option 1</p>
         </div>
         <div className={"first-impressions-card-right"}>
-
+          <p>Option 1</p>
         </div>
         <div className={"first-impressions-card-right"}>
-
+          <p>Option 1</p>
         </div>
         <div className={"first-impressions-card-right"}>
-
+          <p>Option 1</p>
         </div>
       </div>
     </div>
