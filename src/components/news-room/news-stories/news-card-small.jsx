1361007ff580774913a9b6ee985424b99a820a41
Elijah <aa1719@imperial.ac.uk>
2022-10-30 16:13:34 +0000

    Frontend: Fixed news-room.

--- src/components/news-room/news-stories/news-card-small.jsx
+++ src/components/news-room/news-stories/news-card-small.jsx
@@ -4,7 +4,7 @@ import "../news-room.css";
 function NewsCardSmall({ content }) {
   return (
     <a className={"news-card-small"} href={content.link}>
-      <div className={"news-card-small-time"}>X Mins</div>
+      <div className={"news-card-small-time"}>Elijah</div>
       <h5 className={"news-card-small-title"}>{content.title}</h5>
       <img
         className={"news-card-small-thumbnail"}
