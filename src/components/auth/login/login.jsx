0afc817320d79fed4df14629080abc30ab407766
Elijah <aa1719@imperial.ac.uk>
2022-11-22 15:32:38 +0000

    Frontend: updated login page.

--- src/components/auth/login/login.jsx
+++ src/components/auth/login/login.jsx
@@ -1,72 +1,31 @@
 import React from "react";
 import "./login.css";
-import GreenButton from "../../button/green-button";
 
 function Login({ changeObjective }) {
   return (
     <div className={"login-home"}>
-      <div className={"login-top"}>
-        <p>Login</p>
-      </div>
-      <div className={"login-bottom"}>
-        <div className={"login-left"}>
-          <p className={"login-main-text"}>Login to Your Account</p>
-          <div className={"login-forum"}>
-            <p>Email Address</p>
-            <input></input>
-            <p>Password</p>
-            <input></input>
-            <br />
-            <br />
-            <GreenButton text={"Login"} style={{ width: "20%" }} />
-            <div className={"login-button"}>
-              <div className={"login-bar"} />
-              <p>Or Continue With</p>
-              <div className={"login-bar"} />
-            </div>
-            <div className={"login-button"}>
-              <div className={"alternative-sign-in"}>
-                <img
-                  className={"alternative-sign-in-image "}
-                  src={require("../../../assets/Icons/Microsoft-Cloud.png")}
-                  alt={"Login with Microsoft"}
-                />
-              </div>
-              <div className={"alternative-sign-in"}>
-                <img
-                  className={"alternative-sign-in-image "}
-                  src={require("../../../assets/Icons/Google-Cloud.png")}
-                  alt={"Login with Google"}
-                />
-              </div>
-              <div className={"alternative-sign-in"}>
-                <img
-                  className={"alternative-sign-in-image "}
-                  src={require("../../../assets/Icons/Facebook-Cloud.png")}
-                  alt={"Login with Facebook"}
-                />
-              </div>
-            </div>
-          </div>
+      <div className={"login-body"}>
+        <p className={"login-title"}>Login to Your Account</p>
+        <div className={"various-authentication"}>
+          <div className={"alt-auth"} />
+          <div className={"alt-auth"} />
+          <div className={"alt-auth"} />
         </div>
-        <div className={"login-right"}>
-          <div className={"login-right-main"}>
-            <img
-              className={"login-page-illustration"}
-              src={require("../../../assets/Illustrations/login.png")}
-              alt={"Login illustration"}
-            />
-            <span>
-              Or switch to{" "}
-              <span
-                className={"switch-text-bold"}
-                onClick={() => changeObjective(false)}
-              >
-                Sign Up
-              </span>
-            </span>
-          </div>
+
+        <div className={"login-divider"}>
+          <div />
+          <p>or</p>
+          <div />
         </div>
+
+        <form className={"login-form"}>
+          <p>email</p>
+          <input className={"login-input"} />
+          <p>password</p>
+          <input className={"login-input"} />
+        </form>
+
+        <button className={"login-button"}>Sign In</button>
       </div>
     </div>
   );
