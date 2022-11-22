93dc575195165e093aa049f934b87b8f12903981
Elijah <aa1719@imperial.ac.uk>
2022-11-22 18:40:18 +0000

    Frontend: Added basic annimation to login page.

--- src/components/auth/login/login.jsx
+++ src/components/auth/login/login.jsx
@@ -4,28 +4,57 @@ import "./login.css";
 function Login({ changeObjective }) {
   return (
     <div className={"login-home"}>
-      <div className={"login-body"}>
-        <p className={"login-title"}>Login to Your Account</p>
-        <div className={"various-authentication"}>
-          <div className={"alt-auth"} />
-          <div className={"alt-auth"} />
-          <div className={"alt-auth"} />
+      <div className={"login-section slide-in"}>
+        <div className={"login-body"}>
+          <p className={"login-title"}>Login to Your Account</p>
+          <span>
+            if you're new sign in <span className={"underline"}>here</span>
+          </span>
+          <div className={"various-authentication"}>
+            <img
+              alt={"Sign in with Facebook"}
+              src={require("../../../assets/Icons/Facebook Circled.png")}
+              className={"alt-auth"}
+            />
+            <img
+              alt={"Sign in with Facebook"}
+              src={require("../../../assets/Icons/Google Plus.png")}
+              className={"alt-auth"}
+            />
+            <img
+              alt={"Sign in with Facebook"}
+              src={require("../../../assets/Icons/LinkedIn.png")}
+              className={"alt-auth"}
+            />
+          </div>
+
+          <div className={"login-divider"}>
+            <div />
+            <p>or</p>
+            <div />
+          </div>
+
+          <form className={"login-form"}>
+            <p>email</p>
+            <input className={"login-input"} />
+            <br />
+            <p>password</p>
+            <input className={"login-input"} />
+          </form>
+          <br />
+
+          <button className={"login-button"}>Sign In</button>
         </div>
-
-        <div className={"login-divider"}>
-          <div />
-          <p>or</p>
-          <div />
+      </div>
+      <div className={"side-body"}>
+        <div className={"side-section"}>
+          <p className={"side-title"}>New Here?</p>
+          <p className={"side-text-body"}>
+            Sign up and learn about dark psychology in a never before seen
+            interactive way!
+          </p>
+          <button className={"side-button"}>Sign Up</button>
         </div>
-
-        <form className={"login-form"}>
-          <p>email</p>
-          <input className={"login-input"} />
-          <p>password</p>
-          <input className={"login-input"} />
-        </form>
-
-        <button className={"login-button"}>Sign In</button>
       </div>
     </div>
   );
