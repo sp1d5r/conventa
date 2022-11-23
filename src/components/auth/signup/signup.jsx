2170e57137db6c22802fe33384113202aeb7f169
Elijah <aa1719@imperial.ac.uk>
2022-11-23 20:16:21 +0000

    Frontend: Updated login annimation.

--- src/components/auth/signup/signup.jsx
+++ src/components/auth/signup/signup.jsx
@@ -1,84 +1,86 @@
 import React from "react";
 import "../login/login.css";
 
-function SignUp({ changeObjective }) {
-  const size = window.innerWidth < 600;
+function SignUp({ changeObjective, initial, size }) {
   const switch_to_sign_up = () => {
     changeObjective(true);
   };
 
   return (
     <div className={"login-home"}>
-      <div className={"login-section slide-in"}>
-        <div className={"login-body"}>
-          <p className={"login-title"}>Create a New Account</p>
-          {size ? (
-            <span>
-              if you already have an account, click{" "}
-              <span
-                className={"underline"}
-                onClick={() => {
-                  switch_to_sign_up();
-                }}
-              >
-                here
+      <div className={"login-section"}>
+        <div
+          className={`login-body slide-in${initial.current ? "-right" : ""}`}
+        >
+          <div className={"login-body"}>
+            <p className={"login-title"}>Create a New Account</p>
+            {size ? (
+              <span>
+                if you already have an account, click{" "}
+                <span
+                  className={"underline"}
+                  onClick={() => {
+                    switch_to_sign_up();
+                  }}
+                >
+                  here
+                </span>
               </span>
-            </span>
-          ) : (
-            <></>
-          )}
+            ) : (
+              <></>
+            )}
 
-          <div className={"various-authentication"}>
-            <img
-              alt={"Sign in with Facebook"}
-              src={require("../../../assets/Icons/Facebook Circled.png")}
-              className={"alt-auth"}
-            />
-            <img
-              alt={"Sign in with Facebook"}
-              src={require("../../../assets/Icons/Google Plus.png")}
-              className={"alt-auth"}
-            />
-            <img
-              alt={"Sign in with Facebook"}
-              src={require("../../../assets/Icons/LinkedIn.png")}
-              className={"alt-auth"}
-            />
+            <div className={"various-authentication"}>
+              <img
+                alt={"Sign in with Facebook"}
+                src={require("../../../assets/Icons/Facebook Circled.png")}
+                className={"alt-auth"}
+              />
+              <img
+                alt={"Sign in with Facebook"}
+                src={require("../../../assets/Icons/Google Plus.png")}
+                className={"alt-auth"}
+              />
+              <img
+                alt={"Sign in with Facebook"}
+                src={require("../../../assets/Icons/LinkedIn.png")}
+                className={"alt-auth"}
+              />
+            </div>
+
+            <div className={"login-divider"}>
+              <div />
+              <p>or</p>
+              <div />
+            </div>
+
+            <form className={"login-form"}>
+              <p>email</p>
+              <input className={"login-input"} />
+              <br />
+              <p>name</p>
+              <input className={"login-input"} />
+              <br />
+              <p>password</p>
+              <input className={"login-input"} />
+              <br />
+              <p>re-enter password</p>
+              <input className={"login-input"} />
+            </form>
+            <br />
+
+            <button className={"login-button"}>Sign Up</button>
           </div>
-
-          <div className={"login-divider"}>
-            <div />
-            <p>or</p>
-            <div />
-          </div>
-
-          <form className={"login-form"}>
-            <p>email</p>
-            <input className={"login-input"} />
-            <br />
-            <p>name</p>
-            <input className={"login-input"} />
-            <br />
-            <p>password</p>
-            <input className={"login-input"} />
-            <br />
-            <p>re-enter password</p>
-            <input className={"login-input"} />
-          </form>
-          <br />
-
-          <button className={"login-button"}>Sign Up</button>
         </div>
       </div>
       {size ? (
         <></>
       ) : (
-        <div className={"side-body"}>
+        <div className={`side-body ${initial.current ? "slide-in-right" : ""}`}>
           <div className={"side-section"}>
-            <p className={"side-title"}>New Here?</p>
+            <p className={"side-title"}>Have an Account?</p>
             <p className={"side-text-body"}>
-              Sign up and learn about dark psychology in a never before seen
-              interactive way!
+              Sign up and we'll get back to work!
             </p>
             <button
               className={"side-button"}
