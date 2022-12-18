2d2d1b8e5a33da187c1a86b2bfd3e0151e1aefbc
Elijah <aa1719@imperial.ac.uk>
2022-12-18 01:30:00 +0000

    Frontend: Fixed navbar.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,14 @@
 import "./navbar.css";
 import React from "react";
 import { useLocation } from "react-router-dom";
-import TraditionalNavbar from "./traditional-navbar/traditional-navbar";
 import AuthNavbar from "./auth-navbar/auth-navbar";
+import NavbarStandard from "./traditional-navbar/standard-navbar";
 
 function NavBar() {
   const path_name = useLocation().pathname.split("/").at(-1);
   const back_navigation = path_name === "auth" || path_name === "pricing-page";
 
-  return <>{back_navigation ? <AuthNavbar /> : <TraditionalNavbar />}</>;
+  return <>{back_navigation ? <AuthNavbar /> : <NavbarStandard />}</>;
 }
 
 export default NavBar;
