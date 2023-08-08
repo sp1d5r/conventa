e302b85bf0552add5bdeb2c99f1dc30d0021fdda
Elijah <aa1719@imperial.ac.uk>
2023-08-08 08:01:52 +0100

    Caching: Updated service worker to properly cache the offline image.

--- public/placeholder-image.js
+++ public/placeholder-image.js
@@ -51,3 +51,14 @@ function checkLowPowerModePeriodically() {
 setInterval(checkLowPowerModePeriodically, 5000); // 5000 milliseconds = 5 seconds
 
 console.log("Offline caching script loaded");
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch(function (error) {
+      console.log("Service Worker registration failed:", error);
+    });
+}
